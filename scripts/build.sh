aa075122b077848382e464b4f3f8ff1ae4b95a47
George Manning <gmanning@live.co.uk>
2023-07-08 17:09:47 +0100

    setup release action

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,6 @@
-MACOS_TARGET="x86_64-apple-darwin"
-
-echo "Building target for platform ${MACOS_TARGET}"
+echo "Building target for platform x86_64-unknown-linux-gnu"
 echo
+cross build --release --target=x86_64-unknown-linux-gnu
 
 # Add osxcross toolchain to path
 export PATH="$(pwd)/osxcross/target/bin:$PATH"
@@ -14,7 +13,6 @@ export LIBZ_SYS_STATIC=1
 export CC=o64-clang
 export CXX=o64-clang++
 
-cargo build --release --target "${MACOS_TARGET}"
-
+echo "Building target for platform x86_64-apple-darwin"
 echo
-echo Done
+cargo build --release --target x86_64-apple-darwin

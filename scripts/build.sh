5e56ba39e6f49bcd3f96564f2773e89f67a47095
George Manning <gmanning@live.co.uk>
2023-07-08 17:35:57 +0100

    cache deps fix release

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 echo "Building target for platform x86_64-unknown-linux-gnu"
 echo
-cross build --release --target=x86_64-unknown-linux-gnu
+cargo build --release --target=x86_64-unknown-linux-gnu
 
 # Add osxcross toolchain to path
 export PATH="$(pwd)/osxcross/target/bin:$PATH"
